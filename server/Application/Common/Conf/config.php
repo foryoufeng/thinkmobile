1f3f1de82953a4a7b51a73aaa8a21d8aab2b801e
foryoufeng <1906592238@qq.com>
2016-01-19 13:31:59 +0800

    自行修改Common/Conf/config.php文件，配置数据库

--- server/Application/Common/Conf/config.php
+++ server/Application/Common/Conf/config.php
@@ -5,12 +5,13 @@ return array(
     'DEFAULT_MODULE'     => 'Home',
     'MODULE_DENY_LIST'   => array('Common'),
     'MODULE_ALLOW_LIST'  => array('Home','Admin'),
-    //唯时代的
-   // 'APPID'=>'wx54d5fe4df4be7034',
-    'APPID'=>'wxe9a47826a82c59ea',
-    //'APPSECRET'=>'d4624c36b6795d1d99dcf0547af5443d',
-    'APPSECRET'=>'7a23843f47a104c83177adf333841fb7',
-    'DOMAIN'=>'http://wq.duguying.net',
-    'DEVPOINT'=>'20'//积分除数
+     /* 数据库设置 */
+	'DB_TYPE'   => 'mysql', // 数据库类型
+	'DB_HOST'   => '#', // 服务器地址
+	'DB_NAME'   => '#', // 数据库名
+	'DB_USER'   => '#', // 用户名
+	'DB_PWD'    => '#', // 密码
+    'DB_PORT'               =>  '3306',        // 端口
+    'DB_PREFIX'             =>  '#',    // 数据库表前缀
 
 );
--- server/ThinkPHP/Conf/convention.php
+++ server/ThinkPHP/Conf/convention.php
@@ -53,13 +53,13 @@ return  array(
     'DEFAULT_FILTER'        =>  'htmlspecialchars', // 默认参数过滤方法 用于I函数...
 
     /* 数据库设置 */
-	'DB_TYPE'   => 'mysql', // 数据库类型
-	'DB_HOST'   => '123.56.44.104', // 服务器地址
-	'DB_NAME'   => 'ibaohe', // 数据库名
-	'DB_USER'   => 'root', // 用户名
-	'DB_PWD'    => '123abcwq', // 密码
-    'DB_PORT'               =>  '3306',        // 端口
-    'DB_PREFIX'             =>  'ibh_',    // 数据库表前缀
+	'DB_TYPE'   => '#', // 数据库类型
+	'DB_HOST'   => '#', // 服务器地址
+	'DB_NAME'   => '#', // 数据库名
+	'DB_USER'   => '#', // 用户名
+	'DB_PWD'    => '#', // 密码
+    'DB_PORT'               =>  '#',        // 端口
+    'DB_PREFIX'             =>  '#',    // 数据库表前缀
     'DB_PARAMS'          	=>  array(), // 数据库连接参数    
     'DB_DEBUG'  			=>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
     'DB_FIELDS_CACHE'       =>  true,        // 启用字段缓存
